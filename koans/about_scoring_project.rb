e7a55c16d2a4ffe510a396f2e2fcdbbf5bf68bc2
Steve McDaniel <radarseesradar@gmail.com>
2011-09-26 19:34:36 -0400

    I have expanded my awareness up to koan 202.

--- koans/about_scoring_project.rb
+++ koans/about_scoring_project.rb
@@ -29,8 +29,29 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 #
 # Your goal is to write the score method.
 
+class Scorer
+  def initialize( dice )
+    @dice = dice
+  end
+  
+  def score
+    sum = 0
+    sum += score_die_value( 1, 100, 1000)
+    sum += score_die_value( 5, 50 )
+    [2,3,4,6].inject( sum ) { | sum, die_value | sum + score_die_value( die_value ) }
+  end
+  
+  def score_die_value( die_value, singletons_multiplicand=0, triplets_multiplicand=die_value*100 )
+    occurrences = @dice.count( die_value )
+    triplets = occurrences / 3
+    singletons = occurrences % 3
+    singletons * singletons_multiplicand + triplets * triplets_multiplicand
+  end
+  
+end
+
 def score(dice)
-  # You need to write this method
+  Scorer.new( dice ).score
 end
 
 class AboutScoringProject < EdgeCase::Koan
