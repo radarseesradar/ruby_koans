6b651bdf0313ba0d61b0aef0041dd977efd9d0ff
Steve McDaniel <radarseesradar@gmail.com>
2011-09-26 13:28:25 -0400

    I have expanded my awareness up to koan 155.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -13,10 +13,44 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  # WRITE THIS CODE
-end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+end
+
+class Triangle
+  def initialize( *sides )
+    @sides = sides
+    validate
+    type
+  end
+  
+  def number_of_equal_sides
+    @sides.size - @sides.uniq.size + 1
+  end
+  
+  def type
+    case number_of_equal_sides
+    when 3 then :equilateral
+    when 2 then :isosceles
+    else :scalene
+    end
+  end
+  
+  def validate
+    raise TriangleError if @sides.size != 3 || @sides.any? { | side | side <= 0 }
+    enumerator = @sides.each
+    enumerator.with_index do | this_side, index |
+      other_sides = Array.new( @sides )
+      other_sides.slice!( index )
+      raise TriangleError unless other_sides.inject(&:+) > this_side
+    end
+  end
+  
+end
+
+
+def triangle(a, b, c)
+  Triangle.new(a, b, c).type
 end
