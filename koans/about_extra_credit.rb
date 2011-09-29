b5233a9eb172658cf5a9a35389fff239afb034ca
Steve McDaniel <radarseesradar@gmail.com>
2011-09-29 09:27:38 -0400

    This commit adds a comment that describes an initial high level design for the Greed game.

--- koans/about_extra_credit.rb
+++ koans/about_extra_credit.rb
@@ -6,3 +6,26 @@
 # You already have a DiceSet class and score function you can use.
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
+
+# Player
+#   name
+#   first_in_win_zone?
+# Game
+#   play
+# Round
+#   play
+# Turn
+#   play
+#   prompt
+#   query
+#   roll
+#   over?
+# Accumulator
+#   value
+#   TurnAccumulator
+#     update
+#     accumulable?
+#     clear
+#   GameAccumulator
+#     update
+#     in_win_zone?
